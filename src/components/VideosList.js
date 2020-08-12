7a7f4b54bb24770a003e150f60ff9c26e32571b3
mary joseph <mary_jay@icloud.com>
2020-08-12 21:16:59 +0100

    thumnail added to video search by youtube

--- src/components/VideosList.js
+++ src/components/VideosList.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideosList = {props} => {
-    
-    return <div>{props.videos.length}</div>;
-
+const VideosList = ({ videos }) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem video={video} />;
+  });
+  return <div className='ui relaxed divided list'>{renderedList}</div>;
 };
 
 export default VideosList;
