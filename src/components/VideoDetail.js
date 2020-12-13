74ae2cfd317b80ac3f27aeaf281e17bc2fff3fbb
mary joseph <mary_jay@icloud.com>
2020-12-13 19:13:25 +0000

    styling added

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const VideoDetail = ({ video }) => {
   if (!video) {
-    return <di>Loading..</di>;
+    return <div>Loading...</div>;
   }
-  return <div>{video.snippet.title}</div>;
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  return (
+    <div className='ui segment'>
+      <div className='ui embed'>
+        <iframe title='video player' src={videoSrc} />
+      </div>
+      <h4 className='ui header'>{video.snippet.title}</h4>
+      <p>{video.snippet.description}</p>
+    </div>
+  );
 };
 
 export default VideoDetail;
--- src/components/VideosList.js
+++ src/components/VideosList.js
@@ -3,7 +3,13 @@ import VideoItem from './VideoItem';
 
 const VideosList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
   return <div className='ui relaxed divided list'>{renderedList}</div>;
 };
